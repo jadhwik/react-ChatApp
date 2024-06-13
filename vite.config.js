e4d5ec50122415c036dc129314c5f3b927417b16
jadhwik <adhwikjagadeesh@gmail.com>
2024-06-13 12:02:31 +0530

    fles saved

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  base:"/react-chatApp/",
+  plugins: [
+    react()],
   build: {
     rollupOptions: {
       output: {
