58ff8ceacf6f1bdb30dd5e452a8a4b52036e9398
jadhwik <adhwikjagadeesh@gmail.com>
2024-06-13 11:50:00 +0530

    files saved

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,30 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+          if (id.includes('chart.js') || id.includes('d3')) {
+            return 'charting';
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // Increase this value if needed
+  },
 })
