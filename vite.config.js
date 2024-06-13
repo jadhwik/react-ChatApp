da73d3d16088d9f7ceb188fe170789d84762efa1
jadhwik <adhwikjagadeesh@gmail.com>
2024-06-13 13:19:40 +0530

    files saved

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base:"/react-ChatApp/",
+  base:"/login/",
   plugins: [
     react()],
   build: {
